4195b8b23fb68612f43165c4d1e092490dab699d
Jward76 <jhameirward@icloud.com>
2024-08-07 13:45:06 -0700

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Philippine National Police","PapaViska"];
+const statusMessages = ["British Army","PapaViska is daddy"];
 
 
 let currentIndex = 0;
